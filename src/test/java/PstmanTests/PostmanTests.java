da2779e18074babda5e4c72dfb974ebba3e300a0
Artem46125 <poxilove@yandex.ru>
2025-03-27 15:01:52 +0300

    Исправлено

--- src/test/java/PstmanTests/PostmanTests.java
+++ src/test/java/PstmanTests/PostmanTests.java
@@ -23,52 +23,44 @@ public class PostmanTests {
         response.then().body("args", is(emptyMap()));
         response.then().body("headers", is(notNullValue()));
     }
-
     @Test
     public void testPostRequest() {
         String requestData = "{\"test\": \"value\"}";
         Response response = given()
-                .header("Content-Type", "text/plain") // Устанавливаем заголовок Content-Type
-                .body(requestData) // Устанавливаем тело запроса
+                .header("Content-Type", "text/plain")
+                .body(requestData)
                 .when()
                 .post(Base_url + "/post")
                 .then()
-                .statusCode(200) // Проверяем, что код ответа 200
+                .statusCode(200)
                 .extract()
                 .response();
         response.then().body("url", equalTo(Base_url + "/post"));
         response.then().body("data", equalTo(requestData));
         response.then().body("files", is(emptyMap()));
-        response.then().body("form", is(emptyMap()));// Проверяем, что данные соответствуют отправленным
-        response.then().body("args", is(emptyMap())); // Проверяем, что args пустое
-        response.then().body("headers", is(notNullValue())); // Проверяем, что headers не null
-
+        response.then().body("form", is(emptyMap()));
+        response.then().body("args", is(emptyMap()));
+        response.then().body("headers", is(notNullValue()));
     }
-
     @Test
     public void testPutRequest() {
         String requestData = "This is expected to be sent back as part of response body.";
-
-        // Sending the PUT request and getting the response
         Response response = given()
-                .header("Content-Type", "text/plain") // Set the Content-Type header
-                .body(requestData) // Set the request body
+                .header("Content-Type", "text/plain")
+                .body(requestData)
                 .when()
                 .put(Base_url + "/put")
                 .then()
-                .statusCode(200) // Check that the response code is 200
+                .statusCode(200)
                 .extract()
                 .response();
-
-        // Checking the response body
         response.then().body("url", equalTo(Base_url + "/put"));
-        response.then().body("data", equalTo(requestData)); // Verify that the data matches what was sent
+        response.then().body("data", equalTo(requestData));
         response.then().body("args", is(emptyMap()));
         response.then().body("files", is(emptyMap()));
-        response.then().body("form", is(emptyMap()));// Check that args is empty
+        response.then().body("form", is(emptyMap()));
         response.then().body("headers", is(notNullValue()));
-    }// Ensure headers is not null
-
+    }
     @Test
     public void testDeleteRequest() {
         Response response = given()
@@ -78,7 +70,6 @@ public class PostmanTests {
                 .statusCode(200)
                 .extract()
                 .response();
-
         response.then().body("url", equalTo(Base_url + "/delete"));
         response.then().body("args", is(emptyMap()));
         response.then().body("files", is(emptyMap()));
@@ -88,7 +79,6 @@ public class PostmanTests {
     @Test
     public void testPatchRequest() {
         String requestData = "This is expected to be sent back as part of response body.";
-
         Response response = given()
                 .body(requestData)
                 .when()
@@ -97,13 +87,11 @@ public class PostmanTests {
                 .statusCode(200)
                 .extract()
                 .response();
-
         response.then().body("url", equalTo(Base_url + "/patch"));
         response.then().body("args", is(emptyMap()));
         response.then().body("files", is(emptyMap()));
         response.then().body("form", is(emptyMap()));
         response.then().body("headers", is(notNullValue()));
         response.then().body("data", equalTo(requestData));
-
     }
 }
