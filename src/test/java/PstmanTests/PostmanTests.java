eb3c50c87d00cb9a1f965e019112c56042da8bc9
Artem46125 <poxilove@yandex.ru>
2025-03-27 14:55:27 +0300

    Исправил ошибку в файл

--- src/test/java/PstmanTests/PostmanTests.java
+++ src/test/java/PstmanTests/PostmanTests.java
@@ -26,32 +26,48 @@ public class PostmanTests {
 
     @Test
     public void testPostRequest() {
+        String requestData = "{\"test\": \"value\"}";
         Response response = given()
+                .header("Content-Type", "text/plain") // Устанавливаем заголовок Content-Type
+                .body(requestData) // Устанавливаем тело запроса
                 .when()
                 .post(Base_url + "/post")
                 .then()
-                .statusCode(200)
+                .statusCode(200) // Проверяем, что код ответа 200
                 .extract()
                 .response();
         response.then().body("url", equalTo(Base_url + "/post"));
+        response.then().body("data", equalTo(requestData));
+        response.then().body("files", is(emptyMap()));
+        response.then().body("form", is(emptyMap()));// Проверяем, что данные соответствуют отправленным
+        response.then().body("args", is(emptyMap())); // Проверяем, что args пустое
+        response.then().body("headers", is(notNullValue())); // Проверяем, что headers не null
 
     }
 
     @Test
     public void testPutRequest() {
-        Response response = given()
+        String requestData = "This is expected to be sent back as part of response body.";
 
+        // Sending the PUT request and getting the response
+        Response response = given()
+                .header("Content-Type", "text/plain") // Set the Content-Type header
+                .body(requestData) // Set the request body
                 .when()
                 .put(Base_url + "/put")
                 .then()
-                .statusCode(200)
+                .statusCode(200) // Check that the response code is 200
                 .extract()
                 .response();
 
-            // Проверка тела ответа
+        // Checking the response body
         response.then().body("url", equalTo(Base_url + "/put"));
-
-    }
+        response.then().body("data", equalTo(requestData)); // Verify that the data matches what was sent
+        response.then().body("args", is(emptyMap()));
+        response.then().body("files", is(emptyMap()));
+        response.then().body("form", is(emptyMap()));// Check that args is empty
+        response.then().body("headers", is(notNullValue()));
+    }// Ensure headers is not null
 
     @Test
     public void testDeleteRequest() {
@@ -64,5 +80,30 @@ public class PostmanTests {
                 .response();
 
         response.then().body("url", equalTo(Base_url + "/delete"));
+        response.then().body("args", is(emptyMap()));
+        response.then().body("files", is(emptyMap()));
+        response.then().body("form", is(emptyMap()));
+        response.then().body("headers", is(notNullValue()));
+    }
+    @Test
+    public void testPatchRequest() {
+        String requestData = "This is expected to be sent back as part of response body.";
+
+        Response response = given()
+                .body(requestData)
+                .when()
+                .patch(Base_url + "/patch")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        response.then().body("url", equalTo(Base_url + "/patch"));
+        response.then().body("args", is(emptyMap()));
+        response.then().body("files", is(emptyMap()));
+        response.then().body("form", is(emptyMap()));
+        response.then().body("headers", is(notNullValue()));
+        response.then().body("data", equalTo(requestData));
+
     }
 }
